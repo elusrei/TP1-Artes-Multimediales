7a32161ab2a96931e48e1654b01b45703e53874d
elusrei <133175086+elusrei@users.noreply.github.com>
2023-06-06 11:18:49 -0300

    Create sketch.js
    js

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(512, 512);
+  background(220);
+}
+
+function draw() {
+  push();  
+  translate(0,0);
+  for(var i, i <= 1, i++)
+    {
+      rect(100,100);
+    };
+  
+  pop();
+  
+}
